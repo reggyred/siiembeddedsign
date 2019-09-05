95f7ea1afad63ba121ac28626e27c7c7cbe65308
Piotr Reddig <piotrreddig@gmail.com>
2019-09-05 18:22:19 +0200

    WIP1

--- src/embled/demo1.py
+++ src/embled/demo1.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from colors import RGB
 from led_driver import LedDriver
 import numpy
@@ -36,3 +37,52 @@ try:
             sleep(1.0/120)
 except KeyboardInterrupt:
     pass
+=======
+from colors import RGB
+from led_driver import LedDriver
+import numpy
+from colorsys import hsv_to_rgb
+from time import sleep
+import collections
+
+
+def generate_rainbow(number):
+    data = []
+    h = 0.0
+    for i in range(number):
+        r, g, b = hsv_to_rgb(h, 1.0, 0.05)
+        data.append(RGB(r, g, b))
+        h += 1.0/number
+        if h > 1:
+            h = 0.0
+    return data
+
+
+def rotate_list(lst, n):
+    d = collections.deque(lst)
+    d.rotate(n)
+    return list(d)
+
+letter_e_cripple = 53
+letter_d = 70
+letter_e = 56
+letter_m = 51
+letter_b = letter_d
+
+NO_OF_LEDS = letter_e + letter_m + letter_b + letter_e + \
+        + letter_d + letter_d + letter_e + letter_d
+#NO_OF_LEDS = letter_e_cripple + letter_m
+driver = LedDriver()
+source = generate_rainbow(NO_OF_LEDS)
+
+try:
+    while True:
+        for n in range(NO_OF_LEDS):
+            data = rotate_list(source, n)
+            print("Iter")
+            driver.transmit(data)
+            driver.reset_signal()
+            #sleep(1.0/NO_OF_LEDS)
+except KeyboardInterrupt:
+    pass
+>>>>>>> WIP1
--- src/embled/demo2.py
+++ src/embled/demo2.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from colors import RGB
 from led_driver import LedDriver
 import numpy
@@ -23,3 +24,30 @@ try:
         sleep(1/10)
 except KeyboardInterrupt:
     pass
+=======
+from colors import RGB
+from led_driver import LedDriver
+import numpy
+from colorsys import hsv_to_rgb
+from time import sleep
+
+driver = LedDriver()
+
+h = 0.0
+
+point1 = RGB(127, 0, 0)
+point2 = RGB(0, 127, 0)
+point3 = RGB(0, 0, 127)
+blank = RGB(1, 1, 1)
+source = [point1, point2, point3, blank, blank, blank, blank, blank, blank, blank, blank, blank]
+try:
+    while True:
+        data = []
+        for i in range(12):
+            data.extend(source[i:12])
+            data.extend(source[0:i])
+            driver.transmit(data)
+        sleep(1/10)
+except KeyboardInterrupt:
+    pass
+>>>>>>> WIP1
